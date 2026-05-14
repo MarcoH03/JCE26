a25aad4076fe0025e666221ef1305b84c2df8023
MarcoH03 <marcocanizarestan0331@gmail.com>
2026-05-13 20:59:39 -0400

    cambios menores en el orden y funciones nuevas para simplificar el trabajo en el main

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import tools as t
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.sparse as sp
+from matplotlib.animation import FuncAnimation
+
 
 N_R = t.N_R #number of points per branch of the ring
 N_l = t.N_l
@@ -10,116 +12,21 @@ Ef = 4.19 #Fermi Energy in meV
 theta = 12.5*np.pi 
 
 #k= np.sqrt(2*t.m*Ef)/t.h_bar #wavevector of the initial wavefunction in the left lead
-#k = theta/(2*np.pi*t.R)
-k = 100
+k = theta/(2*np.pi*t.R)
 
 
 theta = k * (2*np.pi*t.R)
 print(f"theta = {theta:.2f} rad")
 print(f"k = {k:.4f} nm^-1")
 
-A, B, size = t.matrix_A_B_generator_single_ring(N_R, k)
-A = sp.csr_matrix(A)
-B = sp.csr_matrix(B)
-time_steps = 10000
+time_steps = 1000
 
-
-
-psi_array = np.empty(time_steps+1, dtype=object)
-
-#The wavefunction at the current (known) time step
-psi_old = np.zeros(size, dtype=complex)
-
-for j in range(N_l):
-    x = j
-
-    idx_up = 2*j
-    idx_down = 2*j + 1
-
-    psi_old[idx_up] = np.exp(-(x-2)**2) * np.exp(1j*k*x)
-    psi_old[idx_down] = 0
-    
-psi_array[0] = psi_old
-
-d = B @ psi_old
-
-for i in range(time_steps):
-    psi_new = sp.linalg.spsolve(A, d)
-    psi_array[i+1] = psi_new
-
-    psi_old = psi_new
-    d = B @ psi_old
+#region Solve QR
+psi_up_array, psi_down_array = t.solve_QR(N_R, k, time_steps)
+#endregion Solve QR
 
 #region plot_density
-def extract_density(psi, N_l, N_R):
-    """
-    Convierte el vector interleaved en densidad física ordenada:
-    L → U → D → R
-    """
-
-    def up(j): return 2*j
-    def down(j): return 2*j + 1
-
-    density = []
-
-    # --- LEFT LEAD ---
-    for j in range(N_l):
-        density.append(
-            np.abs(psi[up(j)])**2 + np.abs(psi[down(j)])**2
-        )
-
-    # --- UPPER ARM ---
-    offset = N_l
-    for j in range(N_R):
-        idx = offset + j
-        density.append(
-            np.abs(psi[up(idx)])**2 + np.abs(psi[down(idx)])**2
-        )
-
-    # --- LOWER ARM ---
-    offset = N_l + N_R
-    for j in range(N_R):
-        idx = offset + j
-        density.append(
-            np.abs(psi[up(idx)])**2 + np.abs(psi[down(idx)])**2
-        )
-
-    # --- RIGHT LEAD ---
-    offset = N_l + 2*N_R
-    for j in range(N_l):
-        idx = offset + j
-        density.append(
-            np.abs(psi[up(idx)])**2 + np.abs(psi[down(idx)])**2
-        )
-
-    return np.array(density)
-
-from matplotlib.animation import FuncAnimation
-import matplotlib.pyplot as plt
-import numpy as np
-
-fig, ax = plt.subplots()
-
-# número total de nodos físicos
-N_total = N_l + 2*N_R + N_l
-
-x = np.arange(N_total)
-
-line, = ax.plot(x, np.zeros(N_total))
-
-ax.set_ylim(0, 2)
-
-def update(frame):
-    psi = psi_array[frame]
-
-    rho = extract_density(psi, N_l, N_R)
-
-    line.set_ydata(rho)
-    return (line,)
-
-ani = FuncAnimation(fig, update, frames=len(psi_array), interval=10, blit=True)
-
-plt.show()
+t.plot_animate_psi_total(psi_up_array, psi_down_array, (2*N_l+2*N_R), k)
 #endregion plot_density
 
 #region plot_V
@@ -152,4 +59,15 @@ plt.legend()
 plt.show()
 
 #endregion plot_V
+
+for i in range(100):
+    k = i/100 + 100
+    psi_k_up_array, psi_k_down_array = t.solve_QR(N_R, k, time_steps)
+    t.plot_animate_psi_total(psi_k_up_array, psi_k_down_array, (2*N_l+2*N_R), k)
+    
+
+    
+    
+
+    
 
--- tools.py
+++ tools.py
@@ -1,4 +1,9 @@
 import numpy as np
+import scipy.sparse as sp
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+
 
 #Universal Constants:
 h_bar = 0.658212  # meV*ps
@@ -9,12 +14,12 @@ R = 250 #radious of the ring in nm
 L_leads = 100 #length of the leads in nm
 L_ring = np.pi*R #length of the branch of the ring in nm
 
-N_l = 20 #number of points per lead:
-N_R = 100 #number of points per branch of the ring
+N_l = 5 #number of points per lead:
+N_R = 5 #number of points per branch of the ring
 
 delta_x = L_leads/(N_l-1) #space step for the leads
 delta_s = L_ring/(N_R-1) #space step for the ring
-dt = 1  # ps
+dt = 5  # ps
 
 Phi = 1 / 2 # Aharonov-Bohm Phase
 phi_link = Phi / 2*(N_R-1) #fase de AB agregada en cada paso espacial
@@ -50,7 +55,7 @@ s0_R = 0.2 * L_leads
 
 #potential function for the entire system
 def V(section, i):
-
+    
     # QPC left lead
     if section == "L":
         s = i*delta_x
@@ -445,4 +450,68 @@ def matrix_A_B_generator_single_ring(N_R, k):     #N_R is the numberof points pe
 
     return A,B, size
 
+def solve_QR(N_R, k, time_steps):
+    A, B, size = matrix_A_B_generator_single_ring(N_R, k)
+    A = sp.csr_matrix(A)
+    B = sp.csr_matrix(B)
+        
+    psi_array = np.empty(time_steps+1, dtype=object)
+    
+    #The wavefunction at the current (known) time step
+    psi_old = np.zeros(size, dtype=complex)
 
+    #fill initial wave packet with only spin up component 
+    for j in range(N_l):
+        x = j
+
+        idx_up = 2*j
+        idx_down = 2*j + 1
+
+        psi_old[idx_up] = np.exp(-(x-2)**2) * np.exp(1j*k*x)
+        psi_old[idx_down] = 0
+        
+    psi_array[0] = psi_old
+
+    d = B @ psi_old
+
+    #Solve the sistem for all times in time_steps
+    for i in range(time_steps):
+        psi_new = sp.linalg.spsolve(A, d)
+        psi_array[i+1] = psi_new
+
+        psi_old = psi_new
+        d = B @ psi_old
+        
+    #separate the wave functions in up and down arrays
+    n_t = time_steps + 1
+    n_half = size // 2
+
+    psi_up_array = np.empty((n_t, n_half), dtype=complex)
+    psi_down_array = np.empty((n_t, n_half), dtype=complex)
+
+    for j in range(n_t):
+        psi = psi_array[j]
+        psi_up_array[j, :] = psi[0::2]
+        psi_down_array[j, :] = psi[1::2]
+            
+    return psi_up_array, psi_down_array
+        
+def plot_animate_psi_total(psi_up_array, psi_down_array, N_total, k):
+    fig, ax = plt.subplots()
+    x = np.arange(N_total)
+    
+    line, = ax.plot(x, np.zeros(N_total))
+
+    ax.set_ylim(0, 2)
+    
+    def update(frame):
+        psi = np.abs(psi_up_array[frame])**2 + np.abs(psi_down_array[frame])**2
+        
+        ax.set_title(f"{k}")
+        line.set_ydata(psi)
+        return (line,)
+
+    ani = FuncAnimation(fig, update, frames=len(psi_up_array), interval=1, blit=True)
+
+    plt.show()
+    
